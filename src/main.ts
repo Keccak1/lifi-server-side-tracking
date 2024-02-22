b1b3d3a95e9a336e5dcaf68b779076c0db089f5c
Marcin Cichocki <marcin.cichocki@blocklabs.eu>
2024-02-22 10:38:57 +0100

    host changed to '0.0.0.0'

--- src/main.ts
+++ src/main.ts
@@ -43,6 +43,6 @@ async function bootstrap() {
   const port = process.env.APP_PORT || 3000;
   loggerService.log(`Listening on port ${port}`);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
